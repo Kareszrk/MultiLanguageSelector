f27f92db2a5e30cfa1093e7fcf029ce128055e14
Karesz <81579751+Kareszrk@users.noreply.github.com>
2022-12-04 10:22:39 +0100

    Necessary file uploads
    In this commit I attach all the required files.

---
+++ languages.js
@@ -0,0 +1,53 @@
+class LanguagePreference {
+    constructor() {
+        // There are all the languages.
+        this.languages = {
+            "hu": {
+                WelcomeText: "Üdv a honlapon",
+                WelcomeParagraph: "Köszönöm, hogy meglátogattad a honlapomat!"
+            },
+            "en": {
+                WelcomeText: "Welcome on the website!",
+                WelcomeParagraph: "Thank you for visiting my website!"
+            }
+        }
+        // We need to know what language the user wants to see.
+        // First we run a check, to determine if this is the first time the user sees our website.
+        // First of first, we check if this user is a returning user or a first time user.
+        if(!localStorage.getItem('alreadyFirstTime')){
+            // He is a first time user.
+            // When this is the first time, we save a localStorage varriable.
+            localStorage.setItem("alreadyFirstTime", true);
+            // Now we will know if we should set a new language for the user or not.
+            // So now, we set a language by his geolocation.
+            localStorage.setItem("preferedLanguage", navigator.language.includes("-") ? navigator.language.split("-")[0] : navigator.language);
+        } // No else case needed, this user was there at least once, so this preference is already set to him.
+        this.reloadAllLanguageResources();
+    }
+    // Reload all langauge resources
+    reloadAllLanguageResources = () => {
+        let selectedLanguage = localStorage.getItem('preferedLanguage') ? localStorage.getItem('preferedLanguage') : "en";
+        let all_language_elements = document.querySelectorAll("[data-languagePartIdentifier]");
+        let i = 0;
+        while(i < all_language_elements.length){
+            all_language_elements[i].innerHTML = this.languages[selectedLanguage][all_language_elements[i].getAttribute("data-languagePartIdentifier")];
+            i++;
+        }
+    }
+
+    // Change language method
+    ChangeClientFrontEndLanguagePreference = (languageTo) => {
+        // Here we check if the selected language exists or not.
+        if(this.languages[languageTo]){
+            // Language exists, so we change the preference to that language.
+            localStorage.setItem("preferedLanguage", languageTo);
+            this.reloadAllLanguageResources();
+        } else {
+            // The selected language does not exist in the languages list.
+            // We now just output an error message to the user.
+            window.alert("The selected language is not supported");
+        }
+    }
+}
+
+let languagePreferencesSystem = new LanguagePreference();
